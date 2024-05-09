eab5614b46fabb8769a6c2de72a7c5bbe4c5ae0f
Khalil <khalil.j.khalil2@gmail.com>
2024-05-09 14:22:26 -0400

    Set Up The Basic pygame window display name,width, height

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import random
 import time
 import pygame
 pygame.init()
+
+WIDTH, HEIGHT = 600, 400
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Aim Trainer')
+
+def main():
